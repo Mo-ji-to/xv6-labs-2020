288ed96dda9e0f07a45619adefe2fa11e08985e1
root <root@DESKTOP-33DTFIJ>
2025-04-22 16:15:34 +0800

    完成了惰性分配第一步，修改sbrk

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,22 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  /*if(growproc(n) < 0)
     return -1;
+    先不做实际分配
+    */
+  struct proc* p = myproc();
+  if(n>0){
+    p->sz += n; //惰性分配 仅改变sz字段
+  }
+  else if(p->sz + n > 0){
+    p->sz = uvmdealloc(p->pagetable,p->sz,p->sz + n);
+  }  //如果是减少内存 还是要马上执行 检查减去内存后是否大于0
+  else{
+    return -1;
+  }
+  
+
   return addr;
 }
 
